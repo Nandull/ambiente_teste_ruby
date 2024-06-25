16432db1d3d5b06af47e5ac54d6fec7d48fb3078
fernando <fediniz41@gmail.com>
2024-06-25 07:22:31 -0300

    primeiro teste

---
+++ lib/primeiro_pacote_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "primeiro_pacote_ruby/version"
+
+module PrimeiroPacoteRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/primeiro_pacote_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PrimeiroPacoteRuby
+  VERSION = "0.1.0"
+end
